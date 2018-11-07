7381b3adf9dde39e120e2e7ff8d580bf525775ba
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-11-07 16:43:29 -0300

    Changing to PreProcess class

--- pre_processing.py
+++ pre_processing.py
@@ -4,39 +4,52 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.preprocessing import MinMaxScaler
 
-def pre_process(dataset, experiment, rows=[], columns=[], normalization=False):
-    ds = { #Available datasets
-        'iris': pd.DataFrame(datasets.load_iris().data[:, :]),
-        'wine': pd.DataFrame(datasets.load_wine().data[:, :]),
-        # 'alzheimer': pd.read_csv("datasets/dataPhDAlzheimerSemNomes.csv")
+class PreProcess():
+
+    def __init__(self, experiment):
+        self.experiment = experiment
+        self.ds = {  # Available datasets
+            'iris': pd.DataFrame(datasets.load_iris().data[:, :]),
+            'wine': pd.DataFrame(datasets.load_wine().data[:, :]),
+            'acs-education': pd.read_csv("datasets/df_AcsReadEducation_county_16_5.csv")
+            # 'alzheimer': pd.read_csv("datasets/dataPhDAlzheimerSemNomes.csv")
         }
-
-    if rows and columns: #Select rows and columns
-        data = ds[dataset].iloc[rows, columns]
-    elif rows:
-        data = ds[dataset].iloc[rows, :]
-    elif columns:
-        data = ds[dataset].iloc[:, columns]
-    else:
-        data = ds[dataset].iloc[:, :]
     
-    if normalization: #Normalize dataset
-        std = MinMaxScaler()
-        data = std.fit_transform(data.values)
-        data = pd.DataFrame(data)
-    
-    data_path = f"booking/{experiment}"
-    if not os.path.exists(data_path): #Create directory of experiment
-        os.mkdir(data_path)
+    @property
+    def exp_path(self):
+        return f"booking/{self.experiment}"
 
-    data.to_csv(f"{data_path}/data.csv")#Save data
+    def initialize(self, ds_name, rows=[], columns=[], normalization=False):
+        
+        self.data = self.selectData(ds_name, rows, columns)
+        
+        if normalization: #Normalize dataset
+            std = MinMaxScaler()
+            self.data = std.fit_transform(self.data.values)
+            self.data = pd.DataFrame(self.data)
+        
+        if not os.path.exists(self.exp_path): #Create directory of experiment
+            os.mkdir(self.exp_path)
 
-    config = pd.DataFrame()
-    config['dataset'] = [dataset]
-    config['rows'] = [rows]
-    config['columns'] = [columns]
-    config['normalization'] = [normalization]
-    
-    config.to_csv(f"{data_path}/config.csv")#Save data configuration
-    
+        self.data.to_csv(f"{self.exp_path}/data.csv")#Save data
 
+        config = pd.DataFrame()
+        config['dataset'] = [ds_name]
+        config['rows'] = [rows]
+        config['columns'] = [columns]
+        config['normalization'] = [normalization]
+        
+        config.to_csv(f"{self.exp_path}/config.csv")#Save data configuration
+        
+
+    def selectData(self, ds_name, rows, columns):
+        if rows and columns:  # Select rows and columns
+            data = self.ds[ds_name].iloc[rows, columns]
+        elif rows:
+            data = self.ds[ds_name].iloc[rows, :]
+        elif columns:
+            data = self.ds[ds_name].iloc[:, columns]
+        else:
+            data = self.ds[ds_name].iloc[:, :]
+
+        return data
